af8aff5143d2bfaec76420fa98acfa604bf5b82e
Maris Usis <marisusis0@protonmail.com>
2026-05-25 23:07:15 -0400

    idiomatic rust: in-progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use rand::{RngExt, SeedableRng};
-use std::{mem::MaybeUninit, ptr::NonNull};
+use rand::SeedableRng;
+use std::{intrinsics::mir::PtrMetadata, mem::MaybeUninit, ptr::NonNull};
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -14,17 +14,25 @@ pub fn add(left: u64, right: u64) -> u64 {
 type NodePointer<K, V> = Option<NonNull<Node<K, V>>>;
 
 struct Node<K, V> {
-    key: K,
-    value: V,
+    key: MaybeUninit<K>,
+    value: MaybeUninit<V>,
     forward: Box<[NodePointer<K, V>]>,
 }
 
 impl<K, V> Node<K, V> {
     fn new(key: K, value: V, forward: &[NodePointer<K, V>]) -> Self {
         Node {
-            key,
-            value,
+            key: MaybeUninit::new(key),
+            value: MaybeUninit::new(value),
             forward: Box::from(forward),
+        }
+    }
+
+    fn new2(key: K, value: V, levels: usize) -> Self {
+        Node {
+            key: MaybeUninit::new(key),
+            value: MaybeUninit::new(value),
+            forward: vec![None; levels].into_boxed_slice(),
         }
     }
 
@@ -38,19 +46,19 @@ impl<K, V> Node<K, V> {
     }
 }
 
-impl<K, V> Default for Node<K, V>
-where
-    K: Default,
-    V: Default,
-{
-    fn default() -> Self {
-        Self {
-            key: Default::default(),
-            value: Default::default(),
-            forward: Default::default(),
-        }
-    }
-}
+// impl<K, V> Default for Node<K, V>
+// where
+//     K: Default,
+//     V: Default,
+// {
+//     fn default() -> Self {
+//         Self {
+//             key: Default::default(),
+//             value: Default::default(),
+//             forward: Default::default(),
+//         }
+//     }
+// }
 
 impl<K, V> std::fmt::Display for Node<K, V>
 where
@@ -90,22 +98,32 @@ where
 
 pub struct SkipList<K, V> {
     len: usize,
-    forward: Vec<NodePointer<K, V>>,
+    head: NonNull<Node<K, V>>,
     rng: rand::rngs::StdRng,
 }
 
 /// Private fields
 impl<K, V> SkipList<K, V> {
     const MAX_LEVEL: usize = 29;
-    const P: f64 = 0.5;
+    const P: f64 = 0.2;
 
     fn random_level(&mut self) -> usize {
         let mut new_level = 1;
-        while self.rng.random::<f64>() < Self::P {
+        while rand::random::<f64>() < Self::P {
             new_level += 1;
         }
 
         new_level.min(Self::MAX_LEVEL)
+    }
+
+    fn node(&self) -> &Node<K, V> {
+        self.as_ref()
+    }
+}
+
+impl<K, V> AsRef<Node<K, V>> for SkipList<K, V> {
+    fn as_ref(&self) -> &Node<K, V> {
+        unsafe { self.head.as_ref() }
     }
 }
 
@@ -113,7 +131,11 @@ impl<K, V> SkipList<K, V> {
     pub fn new() -> Self {
         SkipList {
             len: 0,
-            forward: vec![None; Self::MAX_LEVEL],
+            head: NonNull::from(Box::leak(Box::new(Node {
+                key: MaybeUninit::uninit(),
+                value: MaybeUninit::uninit(),
+                forward: vec![None; Self::MAX_LEVEL].into_boxed_slice(),
+            }))),
             rng: rand::rngs::StdRng::seed_from_u64(123u64),
         }
     }
@@ -123,11 +145,11 @@ impl<K, V> SkipList<K, V> {
     }
 
     pub fn level(&self) -> usize {
-        self.forward.iter().filter(|e| e.is_some()).count().max(1)
+        self.as_ref().level()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.forward.iter().all(|e| e.is_none())
+        self.as_ref().forward.iter().all(|e| e.is_none())
     }
 }
 
@@ -137,7 +159,8 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(
-            self.forward
+            self.as_ref()
+                .forward
                 .iter()
                 .enumerate()
                 .fold("".to_string(), |acc, item| {
@@ -147,7 +170,7 @@ where
                     while let Some(node) = cur_node {
                         let node = unsafe { node.as_ref() };
                         cur_node = &node.forward[i];
-                        temp += &format!("-> {:?}", node.key);
+                        temp += &format!("-> {:?}", unsafe { node.key.assume_init_ref() });
                     }
 
                     acc + &temp + "\n"
@@ -164,28 +187,27 @@ where
 {
     pub fn insert(&mut self, key: K, value: V) {
         // TODO make array
-        let mut update: Vec<MaybeUninit<NonNull<[NodePointer<K, V>]>>> =
-            vec![MaybeUninit::uninit(); Self::MAX_LEVEL];
+        let mut update = vec![self.head; Self::MAX_LEVEL];
         let level = self.level();
-        let mut x_forward = self.forward.as_mut_slice();
+
+        let mut x = self.head;
 
         for i in (0..level).rev() {
-            // TODO unwrap unchecked
-            while let Some(mut ptr) = x_forward[i] {
-                let node = unsafe { ptr.as_mut() };
-                if node.key < key {
-                    x_forward = node.forward.as_mut();
+            while let Some(next_node) = unsafe { x.as_ref() }.forward[i] {
+                if *unsafe { next_node.as_ref().key.assume_init_ref() } < key {
+                    x = next_node;
                 } else {
                     break;
                 }
             }
-            update[i].write(x_forward.into());
+
+            update[i] = x;
         }
 
-        if let Some(mut x) = x_forward[0]
-            && unsafe { x.as_ref() }.key == key
+        if let Some(mut x) = unsafe { x.as_ref() }.forward[0]
+            && *unsafe { x.as_ref().key.assume_init_ref() } == key
         {
-            unsafe { x.as_mut() }.value = value;
+            unsafe { x.as_mut() }.value.write(value);
         } else {
             let new_level = self.random_level();
             if new_level > self.level() {
@@ -194,51 +216,47 @@ where
                     .skip(self.level())
                     .take(new_level)
                     .for_each(|e| {
-                        e.write(self.forward.as_mut_slice().into());
+                        *e = self.head;
                     });
             }
 
-            let new_node = Box::new(Node::new(key, value, &[None]));
-            let new_node = NonNull::new(Box::leak(new_node));
-            let mut new_forward = Vec::with_capacity(self.level());
+            let mut new_node =
+                NonNull::from(Box::leak(Box::new(Node::new2(key, value, new_level))));
             for i in 0..new_level {
-                let target_list = unsafe { update[i].assume_init_mut().as_mut() };
+                let mut target_node = update[i];
                 // println!(
                 //     "level {i} new_level {new_level}\nis header {}\n{}",
                 //     target_list.as_ptr() == self.forward.as_ptr(),
                 //     target_list.display_forward()
                 // );
-                let target_node = target_list[i];
-                new_forward.push(target_node);
-                target_list[i] = new_node;
+                unsafe { new_node.as_mut() }.forward[i] =
+                    unsafe { target_node.as_ref() }.forward[i];
+                unsafe { target_node.as_mut() }.forward[i] = Some(new_node);
             }
-            let new_node = unsafe { new_node.unwrap().as_mut() };
-            new_node.forward = new_forward.into_boxed_slice();
+            self.len += 1;
         }
-        self.len += 1;
     }
 
     pub fn search(&self, key: K) -> Option<(K, &V)> {
         let level = self.level();
-        let mut x_forward = self.forward.as_slice();
+
+        let mut x = self.head;
 
         for i in (0..level).rev() {
-            // println!("[forward list]\n{}", x_forward.display_forward());
-            while let Some(ptr) = x_forward[i] {
-                let node = unsafe { ptr.as_ref() };
-                if node.key < key {
-                    x_forward = node.forward.as_ref();
+            while let Some(next_node) = unsafe { x.as_ref() }.forward[i] {
+                if *unsafe { next_node.as_ref().key.assume_init_ref() } < key {
+                    x = next_node;
                 } else {
                     break;
                 }
             }
         }
 
-        match x_forward[0] {
+        match unsafe { x.as_ref() }.forward[0] {
             Some(x) => {
                 let x = unsafe { x.as_ref() };
-                if x.key == key {
-                    Some((key, &x.value))
+                if *unsafe { x.key.assume_init_ref() } == key {
+                    Some((key, unsafe { x.value.assume_init_ref() }))
                 } else {
                     None
                 }
@@ -247,46 +265,40 @@ where
         }
     }
 
-    pub fn delete(&mut self, key: K) -> Result<V, NotFound> {
-        // TODO make array
-        let mut update: Vec<MaybeUninit<NonNull<[NodePointer<K, V>]>>> =
-            vec![MaybeUninit::uninit(); Self::MAX_LEVEL];
+    pub fn delete(&mut self, key: K) -> Option<V> {
+        let mut update = vec![self.head; Self::MAX_LEVEL];
         let level = self.level();
-        let mut x_forward = self.forward.as_mut_slice();
+
+        let mut x = self.head;
 
         for i in (0..level).rev() {
-            // TODO unwrap unchecked
-            while let Some(mut ptr) = x_forward[i] {
-                let node = unsafe { ptr.as_mut() };
-                if node.key < key {
-                    x_forward = node.forward.as_mut();
+            while let Some(next_node) = unsafe { x.as_ref() }.forward[i] {
+                if *unsafe { next_node.as_ref().key.assume_init_ref() } < key {
+                    x = next_node;
                 } else {
                     break;
                 }
             }
-            update[i].write(x_forward.into());
+
+            update[i] = x;
         }
 
-        if let Some(x) = x_forward[0]
-            && unsafe { x.as_ref() }.key == key
+        if let Some(x) = unsafe { x.as_ref() }.forward[0]
+            && *unsafe { x.as_ref().key.assume_init_ref() } == key
         {
             for i in 0..level {
-                let target_list = unsafe { update[i].assume_init_mut().as_mut() };
-                let target_node = target_list[i];
-                if let Some(target_node) = target_node
-                    && target_node.as_ptr() == x.as_ptr()
-                {
-                    target_list[i] = unsafe { x.as_ref() }.forward[i];
-                } else {
-                    break;
-                }
+                let target_node = &mut unsafe { update[i].as_mut() }.forward[0];
+                if let Some(t) = target_node
+                    && t.as_ptr() == x.as_ptr()
+                {}
             }
+
             //let value = unsafe { ManuallyDrop::take(&mut (*x.as_ptr()).value) };
             let x = unsafe { Box::from_raw(x.as_ptr()) };
 
-            Ok(x.value)
+            Some(unsafe { x.value.assume_init() })
         } else {
-            Err(NotFound {})
+            None
         }
     }
 }
@@ -318,9 +330,9 @@ mod tests {
         default_list.insert(3i32, "asdf".into());
         default_list.insert(3i32, "fdsa".into());
         default_list.insert(3i32, "hello, world!".into());
-        println!("{}", default_list.forward.display_forward());
+        println!("{}", default_list.as_ref().forward.display_forward());
         assert!(!default_list.is_empty());
-        assert_eq!(default_list.len(), 3);
+        assert_eq!(default_list.len(), 1);
         let result = default_list.search(3i32);
         assert!(result.is_some());
         let (key, value) = result.unwrap();
@@ -332,14 +344,15 @@ mod tests {
     fn random_inserts() {
         let mut list = SkipList::new();
 
-        let items = 1000;
+        let items = 10;
 
         // TODO Fix collisions
         let mut random_items = HashMap::with_capacity(items);
         for i in 0..items {
-            let key: u64 = rand::random_range(0..3000);
+            let key: u64 = rand::random_range(0..30);
             random_items.insert(key, i);
             list.insert(key, i);
+            eprintln!("{list}");
         }
 
         // Verify we can find all the items
@@ -363,6 +376,7 @@ mod tests {
             deleted_items.push(item);
             eprintln!("deleting {}", item.0);
             list.delete(item.0).unwrap();
+            eprintln!("{list}");
 
             for deleted in deleted_items.iter() {
                 assert!(list.search(deleted.0).is_none(), "item still exists");
@@ -370,6 +384,7 @@ mod tests {
         });
 
         eprintln!("{list}");
+        panic!("WHAT IS HAPPENING");
 
         // empty list
         assert!(list.is_empty());
